b77ffa1f2c716bc1438e4b897a0a5980a39c7b5a
Laurent Peuch <cortex@worlddomination.be>
2013-02-10 21:34:21 +0100

    [fix] creation/modification date of a talk

--- manager/models.py
+++ manager/models.py
@@ -158,7 +158,8 @@ class Talk(models.Model):
     STATUS = ((0, _(u"Waiting")), (1, _(u"Accepted")), (2, _(u"Rejected")),)
     NATURES = (('conference', _(u"Conference")), ('workshop', _(u"Workshop")),)
 
-    date = models.DateField(_(u"Send date"), auto_now=True)
+    created_date = models.DateField(_(u"Submited date"), auto_now_add=True)
+    date = models.DateField(_(u"Last modification date"), auto_now=True)
     status = models.PositiveSmallIntegerField(_(u"Status"), choices=STATUS, default=0)
     topic = models.ForeignKey(Topic)
     title = models.CharField(_(u"Title"), max_length=128)
